9f8b62260cd9dacef67db15ff095bba3a3b520f6
dougmoto <dougmoto@gmail.com>
2026-09-06 21:02:24 -0700

    Add files via upload

--- games.js
+++ games.js
@@ -36,5 +36,41 @@ const GAMES = [
     icon: "VC",
     image: "covers/vampire-crawlers.png",
     color: "#8b1a2b"
+  },
+  {
+    title: "The Case of the Golden Idol",
+    platform: "PS5",
+    file: "golden-idol.html",
+    key: "golden-idol",
+    icon: "GI",
+    image: "covers/golden-idol.png",
+    color: "#c9962e"
+  },
+  {
+    title: "Mina the Hollower",
+    platform: "PS5",
+    file: "mina-hollower.html",
+    key: "mina-hollower",
+    icon: "MH",
+    image: "covers/mina-hollower.png",
+    color: "#8a5cf5"
+  },
+  {
+    title: "1000xRESIST",
+    platform: "PS5",
+    file: "1000xresist.html",
+    key: "1000xresist",
+    icon: "1K",
+    image: "covers/1000xresist.png",
+    color: "#ff3d81"
+  },
+  {
+    title: "Humanity",
+    platform: "PS5",
+    file: "humanity.html",
+    key: "humanity",
+    icon: "HU",
+    image: "covers/humanity.png",
+    color: "#4fc3e0"
   }
 ];
